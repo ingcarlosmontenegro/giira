616d5ab0be9ffb6716fe840f8eed7434362dd3ec
20161020503 <santirian@hotmail.com>
2021-05-04 07:01:20 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,13 +168,14 @@
     });
   });
 
+  /**
 
   // jQuery counterUp
   $('[data-toggle="counter-up"]').counterUp({
     delay: 10,
     time: 1000
   });
-
+*/
 
   // Portfolio details carousel
   $(".portfolio-details-carousel").owlCarousel({
